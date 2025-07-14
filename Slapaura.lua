ab54b4c6d57a11c8110e858d65fbd382a345a953
Easy lua <staskuzmin1406@gmail.com>
2025-07-14 17:09:25 +0300

    Update Slapaura.lua

--- Slapaura.lua
+++ Slapaura.lua
@@ -1,3 +1,1 @@
-local replicated_st = game:GetService("ReplicatedStorage") 
-local gamevent_global = replicated_st.GameEvents
-gamevent_global.CraftingGlobalObjectService:FireServer("SetRecipe", path_craft, "SeedEventWorkbench", "Manuka Flower", 1, 1)  
+
